76e8e79fdf5668a82e75825a258780610fa86430
RODRIGO CARVALHO PEREIRA <rodrigo.cpereira6@senacsp.edu.br>
2020-02-29 15:40:42 -0300

    Adicionamos ao arquivo script os comandos prompt e de exibição

--- aula2/js/script.js
+++ aula2/js/script.js
@@ -1,2 +1,10 @@
 //Vamos criar uma janela de alerta para saudar os usuarios
 alert("Seja bem-vindo");
+/*
+vamos pedir para o usuario digitar o seu nome, e depois mostraremos em uma caixa de alerta.
+Para executar a operação iremos guardar o nome do usuario em uma variável. Essa variável terá o nome de "nome"
+*/
+var nome = prompt("Digite o seu nome: ", "Escreva seu nome aqui");
+alert(nome);
+
+document.getElementById("titulo").innerHTML = nome;
